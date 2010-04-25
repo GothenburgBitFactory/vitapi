386ab2b2afbfa2b5645eefb671516f3ee255e122
Paul Beckingham <paul@beckingham.net>
2010-04-25 15:12:12 -0400

    Utility enhancement - color
    - Now contains correct copyright.
    - Now contains example commands in the usage.
    - Now supports single and blended colors.
    - Indicates when a color is upgraded/interpreted differently.

--- util/color.c
+++ util/color.c
@@ -1,9 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+// VITapi - UI helper library that controls Visuals, Input and Terminals.
+//
+// Copyright 2010, Paul Beckingham, Federico Hernandez.
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the
+//
+//     Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor,
+//     Boston, MA
+//     02110-1301
+//     USA
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <string.h>
 #include <vitapi.h>
 
+////////////////////////////////////////////////////////////////////////////////
 int main (int argc, char** argv)
 {
+  // No arguments - print usage.
   if (argc == 1)
   {
     printf ("\n"
@@ -12,8 +41,16 @@ int main (int argc, char** argv)
             "       color <code>                              Shows color sample by code\n"
             "       color <name>                              Shows color sample by name\n"
             "       color <code>|<name> blend <code>|<name>   Blends two color samples\n"
+            "\n"
+            "For example:\n"
+            "  ./color all\n"
+            "  ./color 1638920\n"
+            "  ./color white on bright red\n"
+            "  ./color white on red blend underline yellow\n"
             "\n");
   }
+
+  // Two arguments, one of which is 'all', so display all available colors.
   else if (argc == 2 && !strcmp (argv[1], "all"))
   {
     char sample[128];
@@ -129,27 +166,98 @@ int main (int argc, char** argv)
 
     printf ("\n");
   }
+
+  // Arguments comprise either a color definition, a color code, or
+  // two colors to be blended.
   else
   {
-    // TODO "./color 123456789"                 -> decode sample
-    // TODO "./color red on blue [blend green]" -> sample
+    char one[128] = {0};
+    char two[128] = {0};
+    int blend = 0;
+    int i;
 
-    color one = atoi (argv[1]);
-/*
-    char name[128];
-    color_
+    // Determine whether there are two colors to blend.
+    for (i = 1; i < argc; ++i)
+    {
+      if (!strcmp (argv[i], "blend"))
+      {
+        blend = i;
+      }
+      else
+      {
+        if (blend == 0)
+        {
+          if (strlen (one) != 0)
+            strcat (one, " ");
 
-    char sample[256];
+          strcat (one, argv[i]);
+        }
+        else
+        {
+          if (strlen (two) != 0)
+            strcat (two, " ");
 
-    std::cout << c1.colorize ("Color Sample: " + ((std::string) c1))
-              << " "
-              << (int) c1
-              << " "
-              << c1.binary ()
-              << std::endl;
-*/
+          strcat (two, argv[i]);
+        }
+      }
+    }
+
+    // Just the one color.
+    if (blend == 0)
+    {
+      char sample[128];
+      char decode[128];
+      char name[128];
+
+      color c = color_def (one);
+      strcpy (sample, "Color sample: ");
+      color_name (name, 128, c);
+      strcat (sample, name);
+      color_colorize (sample, 128, c);
+      color_decode (decode, 128, c);
+
+      printf ("\n%s\nCode:   %d\nDecode: %s\n", sample, c, decode);
+
+      if (strcmp (one, name))
+        printf ("Original color '%s' interpreted as '%s'\n", one, name);
+
+      printf ("\n");
+    }
+
+    // Two colors.
+    else
+    {
+      color c1, c2, c3;
+      char sample1[128], sample2[128], sample3[128];
+      char decode1[128], decode2[128], decode3[128];
+      char name3[128];
+
+      c1 = color_def (one);
+      strcpy (sample1, "Color sample: ");
+      strcat (sample1, one);
+      color_colorize (sample1, 128, c1);
+      color_decode (decode1, 128, c1);
+
+      c2 = color_def (two);
+      strcpy (sample2, "Blend sample: ");
+      strcat (sample2, two);
+      color_colorize (sample2, 128, c2);
+      color_decode (decode2, 128, c2);
+
+      c3 = color_blend (c1, c2);
+      strcpy (sample3, "Blended sample: ");
+      color_name (name3, 128, c3);
+      strcat (sample3, name3);
+      color_colorize (sample3, 128, c3);
+      color_decode (decode3, 128, c3);
+
+      printf ("\n%s\nCode:   %d\nDecode: %s\n",   sample1, c1, decode1);
+      printf ("\n%s\nCode:   %d\nDecode: %s\n",   sample2, c2, decode2);
+      printf ("\n%s\nCode:   %d\nDecode: %s\n\n", sample3, c3, decode3);
+    }
   }
 
   return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////
